da6121664de372b694a0d651e1b46a7a82d4aff8
joshua-ortega07 <josue.ortega1@upr.edu>
2025-09-07 22:00:45 -0400

    Fixed Player Score

--- Game.py
+++ Game.py
@@ -12,7 +12,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 10
+    speed = 5
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -22,12 +22,9 @@ def ball_movement():
     if ball.colliderect(player):
         if abs(ball.bottom - player.top) < 10:  # Check if ball hits the top of the paddle
             # TODO Task 2: Fix score to increase by 1
-            p1_score = 1  # Increase player 1's score
+            p1_score += 1  # Increase player 1's score
             ball_speed_y *= -1  # Reverse ball's vertical direction
             # TODO Task 6: Add sound effects HERE
-            var = pygame.mixer.Sound('file name')
-            pygame.mixer.Sound.play(var)
-
 
     # Ball collision with player 2's paddle
     if ball.colliderect(player2):
@@ -218,6 +215,7 @@ while True:
     screen.blit(p1_hs_txt, (screen_width/2 - 100, 630 + 30))
 
     player2_score = basic_font.render(f'Score: {p2_score}', False, light_grey)
+    p2_hs_txt = basic_font.render(f'Highscore: {p2_score}', False, light_grey)
     screen.blit(player2_score, (screen_width/2 - 65, 40))
     screen.blit(p2_hs_txt, (screen_width/2 - 100, 5))
 
